19e093bb098c911133c7a36742a32aeede3644a6
Ron Dyck <rond@webbtech.net>
2018-10-12 14:06:39 -0400

    upgraded libs

--- App.js
+++ App.js
@@ -26,15 +26,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-        fontSize: 20,   
+    fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
 })
 
 export default () => (
